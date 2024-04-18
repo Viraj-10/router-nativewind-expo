f48bb668f50ebcd4dbf6d56bdf24ffd100ced5e5
Viraj Ajay Joshi <virajajayjoshi@Virajs-MacBook-Pro.local>
2024-04-18 18:01:32 +0530

    intial commit

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -5,7 +5,7 @@ import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function Page() {
   return (
-    <View className="flex flex-1">
+    <View className="flex flex-1 ">
       <Header />
       <Content />
       <Footer />
